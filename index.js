25c8ebe6532ac88cf72c6cc173cacab7dd7a80c5
Mohammed ZAHAF <mohammed.zahaf@gmail.com>
2023-11-05 21:01:05 +0100

    feat(*): graphql setup
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./schema";
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Graphql is amazing!')
+app.get(['/', '/alive'], (req, res) => {
+    res.send('Server is alive!')
 });
+
+const root = {
+    hello: () => {
+        return "I'am Mohammed!";
+    }
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(8585, () => {
     console.log('ZM:: Running server on port localhost:8585/graphql');
-})
+});
